b111399e6a22195d2181460d6b896678e4669745
Rejuwane <rezwanrian1@gmail.com>
2023-11-07 22:10:32 +0600

    extra section complte with react countab

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@ import { Helmet } from "react-helmet-async";
 import Banner from "./Banner/Banner";
 import Section1 from "./Sections/Section1";
 import Cetegary from "./Tab/Cetegary";
+import Section2 from "./Sections/Section2";
 
 
 
@@ -14,6 +15,7 @@ const Home = () => {
            <Banner/>
            <Cetegary/>
            <Section1/>
+           <Section2/>
         </div>
     );
 };
--- src/Components/Home/Sections/Section1.jsx
+++ src/Components/Home/Sections/Section1.jsx
@@ -2,8 +2,13 @@
 import section from '../../../../images/section1.jpg'
 const Section1 = () => {
     return (
-        <div className='mt-4 mb-6'>
+        <div>
+            <div className='mt-4 mb-6'>
             <img src={section} alt="" />
+        </div>
+        <div className='text-center text-4xl font-bold mt-4 mb-4'>
+            <h1>Our Success Story</h1>
+        </div>
         </div>
     );
 };
