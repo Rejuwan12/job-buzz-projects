7790a5f3b92f355833a9681532354c42d43f3eca
Rejuwane <rezwanrian1@gmail.com>
2023-11-09 12:13:49 +0600

    extra feature dark mode added successfully

--- src/Components/MainLayout/MainLayout.jsx
+++ src/Components/MainLayout/MainLayout.jsx
@@ -5,7 +5,7 @@ import Footer from "../Footer/Footer";
 
 const MainLayout = () => {
     return (
-        <div className="max-w-screen-lg mx-auto bg-[#F4F3F0]">
+        <div className="max-w-screen-xl mx-auto bg-[#F4F3F0]">
             <Navbar/>
            <Outlet/>
            <Footer/>
